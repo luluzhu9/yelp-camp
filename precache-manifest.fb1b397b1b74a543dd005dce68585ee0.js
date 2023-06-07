2c11ca5565295883278068f15aeb7b80f5674ebe
Lulu Zhu <luluzhu@Lulus-MBP.T-mobile.com>
2023-06-07 12:21:54 -0700

    Updates

---
+++ precache-manifest.fb1b397b1b74a543dd005dce68585ee0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "46f2b00710122ec2a0f3",
+    "url": "/yelp-camp/static/css/main.cdeb1cd7.chunk.css"
+  },
+  {
+    "revision": "46f2b00710122ec2a0f3",
+    "url": "/yelp-camp/static/js/main.46f2b007.chunk.js"
+  },
+  {
+    "revision": "00760a4a3bb287569c96",
+    "url": "/yelp-camp/static/js/1.00760a4a.chunk.js"
+  },
+  {
+    "revision": "2d8d423118f4917c38e8",
+    "url": "/yelp-camp/static/js/runtime~main.2d8d4231.js"
+  },
+  {
+    "revision": "7fc771056be321e76c3c5f8c8df89981",
+    "url": "/yelp-camp/static/media/Logo.7fc77105.svg"
+  },
+  {
+    "revision": "6cf35507eed5d7b6a314946ea66fdb96",
+    "url": "/yelp-camp/static/media/Hero Image.6cf35507.jpg"
+  },
+  {
+    "revision": "db9a35268465ef13e0d6425ec909b7cf",
+    "url": "/yelp-camp/static/media/Airbnb.db9a3526.svg"
+  },
+  {
+    "revision": "79ea55c1e60373cb00d5739403f87726",
+    "url": "/yelp-camp/static/media/Booking.79ea55c1.svg"
+  },
+  {
+    "revision": "110d5e5b33f63c1dae8da7456eab5c95",
+    "url": "/yelp-camp/static/media/Plum Guide.110d5e5b.svg"
+  },
+  {
+    "revision": "6bf0ff06b9bbffe6e3525b626dbbbcde",
+    "url": "/yelp-camp/static/media/User Testimonial.6bf0ff06.svg"
+  },
+  {
+    "revision": "c42a3305d36254877e6e19e97c219f12",
+    "url": "/yelp-camp/static/media/Map.c42a3305.png"
+  },
+  {
+    "revision": "51d80222ab151abf8a0a1b755e6aa118",
+    "url": "/yelp-camp/static/media/Checkmark.51d80222.svg"
+  },
+  {
+    "revision": "98c13bb838824cf0ab32f75f5d0d949d",
+    "url": "/yelp-camp/index.html"
+  }
+];
